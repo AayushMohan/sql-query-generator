9f57d586b3e465ae743c1d96867b1f8bd49f2cad
AayushMohan <aayushmohan1702@gmail.com>
2023-04-20 19:23:55 +0530

    Added Code Display Styling

--- src/components/CodeDisplay.tsx
+++ src/components/CodeDisplay.tsx
@@ -3,7 +3,18 @@ import React from "react";
 type Props = {};
 
 const CodeDisplay = (props: Props) => {
-  return <div className="code-display">CodeDisplay</div>;
+  return (
+    <div className="code-display">
+      <div className="buttons">
+        <div className="button first"></div>
+        <div className="button middle"></div>
+        <div className="button last"></div>
+      </div>
+      <div className="code-output">
+        <p></p>
+      </div>
+    </div>
+  );
 };
 
 export default CodeDisplay;
